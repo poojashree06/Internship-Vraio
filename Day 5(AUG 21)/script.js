b482ad1b8ac466f28234b7dd6c889e850a2648fb
Poojashree M <poojashreem06@gmail.com>
2023-08-29 11:19:44 +0530

    Updated

--- Day 5(AUG 21)/script.js
+++ Day 5(AUG 21)/script.js
@@ -17,6 +17,10 @@ let tableData = [];
 let savedData = [];
 let i = 0;
 
+/**
+ * The submitAction function validates user input, adds the data to a table, clears the form, sorts the
+ * table, and displays it.
+ */
 function submitAction() {
   if (validate()) {
     let data = {
@@ -37,35 +41,83 @@ function submitAction() {
     searchdiv.setAttribute("class","d-flex mx-4")
   }
 }
-
-function displayTable(obj) {
-  tableBody.innerHTML = "";
-  let sl = 1;
-  obj.forEach((element) => {
-    x = document.createElement("TR");
-    x.setAttribute("id", `row${element.keyValue}`);
-    x.innerHTML = `<tr><td>${sl}</td><td>${element.name}</td><td>${element.regNo}</td><td>${element.grade}</td><td colspan="2"><div class="row g-2 ">
-<div id="update${element.keyValue}"  class="d-none col-6"><button class="btn w-100 px-2 btn-secondary" onclick="updateData(${element.keyValue})">Update</button></div>
-<div id="edit${element.keyValue}" class="d-inline col-6"><button class="btn w-100 btn-primary"  onclick="editData(${element.keyValue})">Edit</button></div>
-<div id="cancel${element.keyValue}" class="d-none col-12"><button class="btn w-100 btn-danger"  onclick="cancelEdit(${element.keyValue})">Cancel</button></div>
-<div id="delete${element.keyValue}" class="d-inline col-6"><button class="btn w-100 btn-danger onclick="deleteData(${element.keyValue})" " >Delete</button></div> </div></td></tr>`;
-    tableBody.appendChild(x);
-    sl++;
-  });
+/**
+ * The function "clearSearchData" clears the search input field and displays the original table data.
+ */
+function clearSearchData()
+{
+  const search=document.getElementById('search').value="";
+    displayTable(tableData)
+  search.innerHTML="";
+  document.getElementById('para').innerHTML=""
+  
 }
 
+/**
+ * The function `displayTable` creates and appends table rows to a table body element based on the data
+ * provided in the `obj` parameter.
+ * @param obj - The `obj` parameter is an array of objects. Each object represents a row of data in a
+ * table. The objects have the following properties:
+ */
+function displayTable(obj) {
+
+  tableBody.innerHTML = ""
+  let sl = 1
+  obj.forEach(element => {
+      x = document.createElement('TR')
+      x.setAttribute('id', `row${element.keyValue}`)
+      x.innerHTML = `<tr>
+      <td>${sl}</td>
+      <td>${element.name}</td>
+      <td>${element.reg}</td>
+      <td>${element.grade}</td>
+      <td colspan="2">
+          <div class="row g-2 ">
+              <div id="update${element.keyValue}" class="d-none col-md-6 ">
+                  <button class="btn w-100  btn-success"  onclick="updateData(${element.keyValue})">Update</button>
+              </div>
+              <div id="edit${element.keyValue}" class="d-block col-md-6 ">
+                  <button class="btn w-100  btn-info"  onclick="editData(${element.keyValue})">Edit</button>
+              </div>
+              <div id="cancel${element.keyValue}" class="d-none col-12 ">
+                  <button class="btn w-100 btn-danger"  onclick="cancelEdit(${element.keyValue})">Cancel</button>
+              </div>
+              <div id="delete${element.keyValue}" class="d-block col-md-6">
+                  <button onclick="deleteData(${element.keyValue})" class="btn  w-100 btn-danger" >Delete</button>
+              </div>
+          </div>
+      </td></tr>`
+      tableBody.appendChild(x)
+      sl++
+  });
+
+  
+}
+
+/**
+ * The `deleteData` function removes an object from the `tableData` array based on a matching
+ * `keyValue` property and then updates the displayed table.
+ * @param keyValue - The `keyValue` parameter is the value that is used to identify the object in the
+ * `tableData` array that needs to be deleted.
+ */
 function deleteData(keyValue) {
   if (!statusMode.localeCompare("normal")) {
-    tableData.splice(
-      tableData.findIndex((object) => {
-        return object.keyValue === keyValue;
-      }),
-      1
-    );
-    displayTable(tableData);
-  }
+    tableData.splice(tableData.findIndex(object => {
+        return object.keyValue === keyValue
+    }), 1)
+    displayTable(tableData)
+}
+if (!tableData.length > 0) {
+    clearTableBtn.style.display = "none"
+}
 }
 
+/**
+ * The function "validate" checks if the username, grade, and registration number fields are empty and
+ * displays appropriate warning messages if they are.
+ * @returns a boolean value. If all the fields (username, grade, and regNo) have values, it will return
+ * true. Otherwise, if any of the fields are blank, it will return false.
+ */
 function validate() {
   if (username.value === "" || grade.value === "" || regNo.value === "") {
     if (grade.value === "") {
@@ -87,6 +139,10 @@ function validate() {
   } else return true;
 }
 
+/**
+ * The function disables the warning message for the username field and sets focus on the username
+ * input.
+ */
 function disableWarning_userName() {
   if(!document.getElementById("username").blur())
   document.getElementById("userNameWarning").innerText =
@@ -94,23 +150,29 @@ function disableWarning_userName() {
         document.getElementById("username").focus()
 }
 
+/**
+ * The function disables the warning message for a registration number.
+ */
 function disableWarning_regNo() {
   regNoWarning.innerHTML = "";
 }
 
+/**
+ * The function disables the warning message for a grade.
+ */
 function disableWarning_grade() {
   
   gradeWarning.innerHTML = "";
   
 }
 
-document.getElementById("grade").onkeydown = function() {
-  var input = parseInt(this.value);
-  if (input < 0 || input > 100)
-    console.log("Value should be between 0 - 100");
-  return;
-}
-
+/**
+ * The `editData` function is used to switch the status mode to "editing" and replace table cells with
+ * input fields for editing data.
+ * @param keyValue - The `keyValue` parameter is a value that identifies the specific data row that
+ * needs to be edited. It is used to target the HTML elements associated with that row and modify their
+ * attributes and content.
+ */
 function editData(keyValue) {
   if (!statusMode.localeCompare("normal")) {
     statusMode = "editing";
@@ -138,6 +200,13 @@ function editData(keyValue) {
   }
 }
 
+/**
+ * The function `onInputChange` updates the status mode to "updating" and displays the update and
+ * cancel buttons based on the input key value.
+ * @param keyValue - The `keyValue` parameter is a value that is used to identify a specific element in
+ * the HTML document. It is typically used as an identifier or key to perform specific actions or
+ * updates on that element.
+ */
 function onInputChange(keyValue) {
   if (!statusMode.localeCompare("editing")) {
     statusMode = "updating";
@@ -150,6 +219,13 @@ function onInputChange(keyValue) {
   }
 }
 
+/**
+ * The `cancelEdit` function cancels the editing mode for a specific row in a table and restores the
+ * original data.
+ * @param keyValue - The `keyValue` parameter is a unique identifier for the row that needs to be
+ * canceled. It is used to locate the specific row in the table and revert the changes made during the
+ * editing process.
+ */
 function cancelEdit(keyValue) {
   statusMode = "normal";
   let row = document.getElementById(`row${keyValue}`);
@@ -173,14 +249,24 @@ function cancelEdit(keyValue) {
     .setAttribute("class", "d-block col-6 justify-content-center");
 }
 
+/**
+ * The function `updateData` updates the data in a table row based on the input value and updates the
+ * corresponding HTML elements.
+ * @param keyValue - The `keyValue` parameter is a unique identifier for the data row that needs to be
+ * updated. It is used to locate the specific row in the table and update its values.
+ */
 function updateData(keyValue) {
   statusMode = "normal";
   let row = document.getElementById(`row${keyValue}`);
+  dataIndex = tableData.findIndex(object => {
+              return object.keyValue === keyValue
+          })
 
   for (i = 1; i < row.cells.length - 1; i++) {
-    inputvalue = document.getElementById(`input${i}`).value;
-    row.cells[i].innerHTML = inputvalue;
-  }
+    inputvalue = document.getElementById(`row${keyValue}`).cells[i].childNodes[0].value
+    tableData[dataIndex][Object.keys(tableData[dataIndex])[i]] = inputvalue;
+    row.cells[i].innerHTML = inputvalue
+}
   document
     .getElementById(`update${keyValue}`)
     .setAttribute("class", "d-none col-6 justify-content-center");
@@ -193,9 +279,14 @@ function updateData(keyValue) {
   document
     .getElementById(`delete${keyValue}`)
     .setAttribute("class", "d-block col-6 justify-content-center");
+
 }
 
-// action on search
+/**
+ * The function `searchData` takes a search value from an input field, filters a table data based on
+ * the search value, and displays the filtered data in a table. If no results are found, it displays a
+ * message indicating that no results were found.
+ */
 function searchData(){
     searchValue = document.getElementById('search').value.toString().trim()
     if (!searchValue.localeCompare("")){
@@ -211,9 +302,6 @@ function searchData(){
         else {
            x=document.getElementById('para')
            if(!x){
-            x = document.createElement('p')
-            x.setAttribute("class","text text-danger")
-            x.setAttribute("id","para")
             x.innerHTML = "No result found "
             tableBody.innerHTML = ""
             mydiv.appendChild(x)
@@ -227,15 +315,26 @@ function searchData(){
       }
 }
 
-// filters the table-data object by search-value
+/**
+ * The function filters an array of objects based on a search key.
+ * @param arr - The `arr` parameter is an array of objects that you want to filter based on a search
+ * key. Each object in the array represents a data entry.
+ * @param searchKey - The searchKey parameter is a string that represents the value you want to search
+ * for in the objects of the array.
+ */
 function filterBySearch(arr, searchKey){
-    return arr.filter((obj) => {
-        return Object.keys(obj).some((keyValue) => {
-            return obj[keyValue].toString().includes(searchKey);
-        })
-    });
+  return arr.filter((obj) => {
+    return Object.keys(obj).some((keyValue) => {
+        return obj[keyValue].toString().includes(searchKey);
+    })
+  });
 }
 
+/**
+ * The function `sortTable` is used to sort a table based on a specific column when called.
+ * @param n - The parameter `n` in the `sortTable` function represents the index of the column that you
+ * want to sort the table by. The sorting will be based on the values in that column.
+ */
 function sortTable(n) {
   let table;
   val++;
@@ -287,23 +386,39 @@ function sortTable(n) {
     x.innerHTML = i;
   }
 }
-function Warning_userName() {
-  userNameWarning.innerHTML = "Enter the Valid Name(Hint Alphabets Only)";
-}
-
+/**
+ * The function "Warning_grade" displays a warning message asking the user to enter numbers from 0 to
+ * 100.
+ */
 function Warning_grade() {
   gardeWarning.innerHTML = "Enter Numbers from 0 to 100";
 }
 
+/**
+ * The function "inputChange" displays the update button.
+ */
 function inputChange() {
   var updateButton = document.getElementById("updateButton");
   updateButton.style.display = "inline-block";
 }
 
+/**
+ * The clearForm function resets the values of all input fields in a form with the id "myForm".
+ */
 function clearForm() {
   document.getElementById("myForm").reset();
 }
 
+/**
+ * The function clearTheTable reloads the current page, effectively clearing the table.
+ */
 function clearTheTable(){
-  location.reload()
+  displayTable(tableData)
+    if (confirm("Alert! This action will clear all data!")) {
+        clearForm()
+        clearSearchData()
+        tableData = []
+        displayTable(tableData)
+        clearButton.style.display = "none"
+    }
 }
